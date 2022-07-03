4669f7c5d47c43802d0f251186046a7ee0f4317e
Djailson1 <djailson_silva1@outlook.com>
2022-07-02 21:28:20 -0300

    erro no parseint?

--- arquivos/src/arquivos/LerArquivoTxt.java
+++ arquivos/src/arquivos/LerArquivoTxt.java
@@ -3,24 +3,43 @@ package arquivos;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LerArquivoTxt {
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		FileInputStream entradaArquivo = new FileInputStream(new File("C:\\Users\\Aluno\\git\\repository12\\arquivos\\src\\arquivos\\arquivo.txt"));
+		FileInputStream entradaArquivo = new FileInputStream(
+				new File("C:\\Users\\Aluno\\git\\repository12\\arquivos\\src\\arquivos\\arquivo.txt"));
 		
 		Scanner lerArquivo = new Scanner(entradaArquivo, "UTF-8");
 		
-		while(lerArquivo.hasNext()) {
+		List<Pessoa> pessoas = new ArrayList<Pessoa>();
+		
+		while(lerArquivo.hasNext()) {/*enquanto tiver dados nesse arquivo*/
 			
 			
-			String linha = lerArquivo.nextLine();
+			String linha = lerArquivo.nextLine();/*minha linha*/
 			
-			if(linha != null && !linha.isEmpty())
-			System.out.println(linha);
+			if(linha != null && !linha.isEmpty()) {/*se a linha for válida*/
+			
+			String[]dados = linha.split("\\;");/*quebra essa linha separando os dados*/
+			/*cada linha depois de instanciada é um novo objeto*/
+			Pessoa pessoa = new Pessoa();
+			pessoa.setEmail(dados[0]);
+			pessoa.setNome(dados[1]);
+			pessoa.setIdade(Integer.parseInt(dados[2]));
+			/*setando as posições na lista*/
+			pessoas.add(pessoa);
+			/*adicionando na lista*/
+				
+			}
+			
 			
 		}
+		
+		System.out.println(pessoas);/*chamando a lista de pessoas*/
 		
 	}
 
